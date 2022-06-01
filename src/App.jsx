8efe3e36cf41a73ebe733ec5c8e95f0a42d3bd62
martsDev <martssilvapro@gmail.com>
2022-06-01 17:13:02 -0300

    add count store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import create from 'zustand';
 
 const App = () => {
+
+  const useStore = create(set => ({
+    count: 0,
+    inc: () => set(state => ({ count: state.count + 1 }))
+  }))
+
+  const inc = useStore(state => state.inc);
+  const num = useStore(state => state.count);
+
   return (
     <div>
-      
+      <div>{num}</div>
+      <br />
+      <button onClick={inc}>Count!</button>
     </div>
   );
 };
